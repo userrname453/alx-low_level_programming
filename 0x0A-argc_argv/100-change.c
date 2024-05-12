bf2d6e0496bde9129e993dc6761956996aa6a5d8
mohamed <akramsboui453@gmail.com>
2024-05-12 10:44:47 +0100

    asdf

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 /**
- * main - prints  min number of coins to make change
+ * main - prints the min number of coins to make change
  * for an amount of money
  * @argc: argument count
  * @argv: arguments
